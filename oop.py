438aed9150784ffa42c31eea6de10309c43036c0
Evgeniy Osipov <z624153@mail.ru>
2022-10-03 14:28:38 +0400

    created new classes and realised new system of grades

--- oop.py
+++ oop.py
@@ -7,23 +7,56 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
  
-        
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)   
+    
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+ 
+     
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        
  
+class Lecturer(Mentor):
+    grades = {}
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.finished_courses += ['Git']
 best_student.courses_in_progress += ['Python']
-best_student.grades['Git'] = [10, 10, 10, 10, 10]
-best_student.grades['Python'] = [10, 10]
  
-print(best_student.finished_courses)
-print(best_student.courses_in_progress)
-print(best_student.grades)
- 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
-print(cool_mentor.courses_attached)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+best_lecturer = Lecturer('Dima', 'Fomin')
+best_lecturer.courses_attached += ['OOP', 'Python']
+
+best_student.rate_lecture(best_lecturer, 'Python', 5)
+
+
+print(cool_mentor.name)
+
+print(f'Лучший студент - {best_student.name} с оценками: {best_student.grades}')
+print(best_lecturer.courses_attached)
+print(f'Уроки лектора {best_lecturer.name} оценили {best_lecturer.grades}')
